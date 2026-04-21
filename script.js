6242ce25703be82b8a80d3d4c01c7c17c46c2f6d
Austin <andersonaustin506@gmail.com>
2026-04-20 19:50:03 -0500

    font fixer

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// Make accessibility tab keyboard useable, I dont know how I forgot to make the accessibility toggle that isnt even needed for this class useable by people who would need it
+document.querySelectorAll(".access-tab").forEach(function(tab) {
+    tab.setAttribute("tabindex", "0");
+    tab.setAttribute("role", "button");
+    tab.addEventListener("keydown", function(e) {
+        if (e.key === "Enter" || e.key === " ") {
+            e.preventDefault();
+            togglePanel();
+        }
+    });
+});
